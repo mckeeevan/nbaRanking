9ef68d9b6c03434666b6fec80bedc5c69e3e98f6
mckeeevan <mckee.evan@gmail.com>
2022-09-23 01:45:47 +0000

    Fixed Elo math

--- pkg/elo/score.go
+++ pkg/elo/score.go
@@ -58,5 +58,5 @@ func scoreChange(player importjson.Player, rating int, win bool) float64 {
 		return float64(rating) + float64(kRate(player))*(1.0-player.ExpectedScore)
 	}
 	// if they lose subract from their score
-	return float64(rating) - float64(kRate(player))*(1.0-player.ExpectedScore)
+	return float64(rating) + float64(kRate(player))*(0.0-player.ExpectedScore)
 }
