5e8f62211c7f6f4ce833ef0a4d381b4425e81390
mckeeevan <mckee.evan@gmail.com>
2022-09-24 21:07:36 +0000

    Winner goes on

--- pkg/matchup/makeMatchup.go
+++ pkg/matchup/makeMatchup.go
@@ -9,55 +9,84 @@ import (
 	importjson "main.go/pkg/importJSON"
 )
 
-func Random(players []importjson.Player) []importjson.Player {
+type Matchup struct {
+	one int
+	two int
+}
 
+func pickMatchup(players []importjson.Player, match Matchup, winner bool) Matchup {
+	if winner {
+		// Add one person to winner
+		two := rand.Intn(len(players))
+		for match.one == two || math.Abs(float64(players[match.one].Elo-players[two].Elo)) > 400 {
+			two = rand.Intn(len(players))
+		}
+		match.two = two
+		return match
+	}
 	one := rand.Intn(len(players))
 	two := rand.Intn(len(players))
 	for two == one || math.Abs(float64(players[one].Elo-players[two].Elo)) > 400 {
 		two = rand.Intn(len(players))
 	}
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
-	displayPlayerInfo(players[one])
-	fmt.Println()
-	displayPlayerInfo(players[two])
-	fmt.Println()
-	fmt.Println()
-	fmt.Println()
+	return Matchup{one, two}
+}
+
+func Random(players []importjson.Player, matchIncoming Matchup, winner bool) ([]importjson.Player, Matchup, bool) {
+	match := pickMatchup(players, matchIncoming, winner)
+
+	displayMatchup(players, match)
 	var pick int
 	fmt.Print("Player 1 or 2: ")
 	fmt.Scanln(&pick)
 
 	if pick == 1 {
-		players[one], players[two] = elo.Elo(players[one], players[two], true)
-		fmt.Println("You chose", players[one].Name, "as being better than", players[two].Name)
-		fmt.Println(players[one].Name, "is now ranked", players[one].Elo)
-		fmt.Println(players[two].Name, "is now ranked", players[two].Elo)
+		players[match.one], players[match.two] = elo.Elo(players[match.one], players[match.two], true)
+		fmt.Println("You chose", players[match.one].Name, "as being better than", players[match.two].Name)
+		fmt.Println(players[match.one].Name, "is now ranked", players[match.one].Elo)
+		fmt.Println(players[match.two].Name, "is now ranked", players[match.two].Elo)
 		// fmt.Println(players)
+
+		return players, match, true
 	}
 	if pick == 2 {
-		players[one], players[two] = elo.Elo(players[one], players[two], false)
-		fmt.Println("You chose", players[two].Name, "as being better than", players[one].Name)
-		fmt.Println(players[one].Name, "is now ranked", players[one].Elo)
-		fmt.Println(players[two].Name, "is now ranked", players[two].Elo)
+		players[match.one], players[match.two] = elo.Elo(players[match.one], players[match.two], false)
+		fmt.Println("You chose", players[match.two].Name, "as being better than", players[match.one].Name)
+		fmt.Println(players[match.one].Name, "is now ranked", players[match.one].Elo)
+		fmt.Println(players[match.two].Name, "is now ranked", players[match.two].Elo)
 		// fmt.Println(players)
+		match.one = match.two
+
+		return players, match, true
 	}
 
-	return players
+	return players, match, false
 }
 
 func displayPlayerInfo(player importjson.Player) {
 	fmt.Println(player.Name, "who in 2021-2022 averaged", "Points:", player.Points, "Assists:", player.Assists, "Rebounds:", player.Rebounds, "Blocks:", player.Blocks, "Games Played:", player.GP, "Games Started:", player.GS,
 		"per game.")
 
+}
+
+func displayMatchup(players []importjson.Player, match Matchup) {
+
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	displayPlayerInfo(players[match.one])
+	fmt.Println()
+	displayPlayerInfo(players[match.two])
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
 }
