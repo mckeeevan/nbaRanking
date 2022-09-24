9179b6d6aa8dd60603d429afb03c8bd99a60d03b
mckeeevan <mckee.evan@gmail.com>
2022-09-24 21:25:13 +0000

    Win Count Matchup Added

--- pkg/matchup/makeMatchup.go
+++ pkg/matchup/makeMatchup.go
@@ -9,31 +9,39 @@ import (
 	importjson "main.go/pkg/importJSON"
 )
 
+const (
+	winsAllowed = 3
+)
+
 type Matchup struct {
 	one int
 	two int
 }
 
-func pickMatchup(players []importjson.Player, match Matchup, winner bool) Matchup {
-	if winner {
+func pickMatchup(players []importjson.Player, match Matchup, winner bool, winCount int) (Matchup, int) {
+	if winner && winCount < winsAllowed {
 		// Add one person to winner
 		two := rand.Intn(len(players))
 		for match.one == two || math.Abs(float64(players[match.one].Elo-players[two].Elo)) > 400 {
 			two = rand.Intn(len(players))
 		}
 		match.two = two
-		return match
+		winCount++
+		return match, winCount
 	}
 	one := rand.Intn(len(players))
 	two := rand.Intn(len(players))
 	for two == one || math.Abs(float64(players[one].Elo-players[two].Elo)) > 400 {
 		two = rand.Intn(len(players))
 	}
-	return Matchup{one, two}
+	return Matchup{one, two}, 0
 }
 
-func Random(players []importjson.Player, matchIncoming Matchup, winner bool) ([]importjson.Player, Matchup, bool) {
-	match := pickMatchup(players, matchIncoming, winner)
+func Random(players []importjson.Player, matchIncoming Matchup, winner bool, winCount int, winChange bool) ([]importjson.Player, Matchup, bool, int, bool) {
+	if winChange {
+		winCount = 0
+	}
+	match, winCount := pickMatchup(players, matchIncoming, winner, winCount)
 
 	displayMatchup(players, match)
 	var pick int
@@ -47,7 +55,7 @@ func Random(players []importjson.Player, matchIncoming Matchup, winner bool) ([]
 		fmt.Println(players[match.two].Name, "is now ranked", players[match.two].Elo)
 		// fmt.Println(players)
 
-		return players, match, true
+		return players, match, true, winCount, false
 	}
 	if pick == 2 {
 		players[match.one], players[match.two] = elo.Elo(players[match.one], players[match.two], false)
@@ -57,10 +65,10 @@ func Random(players []importjson.Player, matchIncoming Matchup, winner bool) ([]
 		// fmt.Println(players)
 		match.one = match.two
 
-		return players, match, true
+		return players, match, true, winCount, true
 	}
 
-	return players, match, false
+	return players, match, false, winCount, true
 }
 
 func displayPlayerInfo(player importjson.Player) {
