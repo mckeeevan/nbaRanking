2dc171456e40b55e4ef9a39fc416fe20f69d6d3e
mckeeevan <mckee.evan@gmail.com>
2022-09-27 14:45:08 +0000

    Split Initialize functions

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	importjson "main.go/pkg/importJSON"
+	"main.go/pkg/initialize"
 	"main.go/pkg/matchup"
 )
 
@@ -22,10 +23,10 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	// Import players for the first time
-	// players := intialize(29.0, 10.0)
+	players := initialize.Players(29.0, 10.0)
 
 	// reopen ranked players
-	players := importjson.Ranked("cmd/scoredPlayers.json")
+	// players := importjson.Ranked("cmd/scoredPlayers.json")
 
 	run(players)
 
@@ -40,64 +41,6 @@ web stuff
 
 */
 
-func getInitialData() ([]importjson.Player, []importjson.Season) {
-	players := importjson.Import("cmd/playerlist.json")
-	seasonData := importjson.ImportSeasons("cmd/playerstats.json")
-
-	return players, seasonData
-}
-
-func intialize(minutes, games float64) []importjson.Player {
-
-	players, seasonData := getInitialData()
-
-	players = combinePlayersAndStats(players, seasonData)
-
-	return minuteAndGameRestricted(players, minutes, games)
-
-}
-
-func combinePlayersAndStats(players []importjson.Player, seasonData []importjson.Season) []importjson.Player {
-
-	for i, j := range players {
-		for _, v := range seasonData {
-			if v.Player == j.Name {
-				players = applyStats(players, v, i)
-
-			}
-			// give all players an initial Elo
-			players[i].Elo = 1000
-		}
-	}
-	return players
-}
-
-func minuteAndGameRestricted(players []importjson.Player, minutes, games float64) []importjson.Player {
-
-	// Only have players with x minutes
-	var minutesRestrictedPlayers []importjson.Player
-
-	// check for players with two few minutes and games
-	for i, v := range players {
-		if v.Minutes > minutes && v.GP > games {
-			minutesRestrictedPlayers = append(minutesRestrictedPlayers, players[i])
-		}
-	}
-
-	return minutesRestrictedPlayers
-}
-
-func applyStats(players []importjson.Player, v importjson.Season, i int) []importjson.Player {
-	players[i].Points = v.Points
-	players[i].Rebounds = v.Rebounds
-	players[i].Assists = v.Assists
-	players[i].Blocks = v.Blocks
-	players[i].GP = v.GP
-	players[i].GS = v.GS
-	players[i].Steals = v.Steals
-	players[i].Minutes = v.Minutes
-	return players
-}
 
 func run(players []importjson.Player) {
 
@@ -114,7 +57,7 @@ func run(players []importjson.Player) {
 	displayRankedPlayers(data)
 
 	// Output data
-	writeData(data, "cmd/scoredPlayers.json")
+	// writeData(data, "cmd/scoredPlayers.json")
 }
 
 func writeData(data matchup.ModelData, fileName string) {
