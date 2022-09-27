fd8b0c7e4fd320a34d32e0d9be07f632c82a8f64
mckeeevan <mckee.evan@gmail.com>
2022-09-27 14:15:27 +0000

    Splitting functions

--- cmd/main.go
+++ cmd/main.go
@@ -13,10 +13,12 @@ import (
 )
 
 const (
-	runs = 50
+	runs = 5
 )
 
 func main() {
+
+	// make random seed
 	rand.Seed(time.Now().UnixNano())
 
 	// Import players for the first time
@@ -49,24 +51,30 @@ func intialize(minutes, games float64) []importjson.Player {
 
 	players, seasonData := getInitialData()
 
-	//players := importjson.Initialize("cmd/players.json")
+	players = combinePlayersAndStats(players, seasonData)
+
+	return minuteAndGameRestricted(players, minutes, games)
+
+}
+
+func combinePlayersAndStats(players []importjson.Player, seasonData []importjson.Season) []importjson.Player {
 
 	for i, j := range players {
 		for _, v := range seasonData {
 			if v.Player == j.Name {
-				players[i].Points = v.Points
-				players[i].Rebounds = v.Rebounds
-				players[i].Assists = v.Assists
-				players[i].Blocks = v.Blocks
-				players[i].GP = v.GP
-				players[i].GS = v.GS
-				players[i].Steals = v.Steals
-				players[i].Minutes = v.Minutes
+				players = applyStats(players, v, i)
+
 			}
 			// give all players an initial Elo
 			players[i].Elo = 1000
 		}
 	}
+
+	return players
+}
+
+func minuteAndGameRestricted(players []importjson.Player, minutes, games float64) []importjson.Player {
+
 	// Only have players with x minutes
 	var minutesRestrictedPlayers []importjson.Player
 
@@ -80,6 +88,18 @@ func intialize(minutes, games float64) []importjson.Player {
 	return minutesRestrictedPlayers
 }
 
+func applyStats(players []importjson.Player, v importjson.Season, i int) []importjson.Player {
+	players[i].Points = v.Points
+	players[i].Rebounds = v.Rebounds
+	players[i].Assists = v.Assists
+	players[i].Blocks = v.Blocks
+	players[i].GP = v.GP
+	players[i].GS = v.GS
+	players[i].Steals = v.Steals
+	players[i].Minutes = v.Minutes
+	return players
+}
+
 func run(players []importjson.Player) {
 
 	// Make first matchup
@@ -90,30 +110,40 @@ func run(players []importjson.Player) {
 	for i := 0; i < runs; i++ {
 		data = matchup.Random(data)
 	}
+
+	data = sortPlayers(data)
+	displayRankedPlayers(data)
+
+	// Output data
+	writeData(data, "cmd/scoredPlayers.json")
+}
+
+func writeData(data matchup.ModelData, fileName string) {
+	var output importjson.Players
+	output.Players = data.Players
+	jsonOutput, _ := json.Marshal(output)
+	_ = ioutil.WriteFile(fileName, jsonOutput, 0644)
+
+}
+func sortPlayers(data matchup.ModelData) matchup.ModelData {
 	// sort the slice
-	sort.Slice(players, func(i, j int) bool {
-		return players[i].Elo > players[j].Elo
+	sort.Slice(data.Players, func(i, j int) bool {
+		return data.Players[i].Elo > data.Players[j].Elo
 	})
 	// print everyone
+	return data
+}
 
+func displayRankedPlayers(data matchup.ModelData) {
 	blankLines(4)
 	for _, v := range data.Players {
 		fmt.Printf("%25s has an elo of %6.0d\n", v.Name, v.Elo)
 	}
-
-	var output importjson.Players
-	output.Players = data.Players
-	jsonOutput, _ := json.Marshal(output)
-	_ = ioutil.WriteFile("cmd/scoredPlayers.json", jsonOutput, 0644)
-}
-
-func displayRankedPlayers() {
 
 }
 
 func blankLines(lines int) {
 	for i := 0; i < lines; i++ {
 		fmt.Println()
-
 	}
 }
