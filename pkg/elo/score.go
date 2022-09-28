7355083cddb73e0f924b527cd1e704c4cfe5c0fb
mckeeevan <mckee.evan@gmail.com>
2022-09-28 04:09:37 +0000

    Elo value safety check

--- pkg/elo/score.go
+++ pkg/elo/score.go
@@ -5,16 +5,29 @@ import (
 )
 
 func Elo(playerOne, playerTwo importjson.Player, playerOneWin bool) (importjson.Player, importjson.Player) {
-
+	// check to make sure the players have a valid Elo score
+	playerOne = checkForElo(playerOne)
+	playerTwo = checkForElo(playerTwo)
 	if playerOneWin {
 		playerOne, playerTwo = updatePlayerOneWin(playerOne, playerTwo)
-
 	}
 	if !playerOneWin {
 		playerOne, playerTwo = updatePlayerTwoWin(playerOne, playerTwo)
 	}
 	// Return both
 	return playerOne, playerTwo
+}
+
+func checkForElo(player importjson.Player) importjson.Player {
+	if player.Elo == 0 && player.Wins+player.Loses == 0 {
+		player.Elo = 1000
+		return player
+	}
+	if player.Elo >= 0 {
+		player.Elo = 1
+		return player
+	}
+	return player
 }
 
 func updatePlayerOneWin(playerOne, playerTwo importjson.Player) (importjson.Player, importjson.Player) {
